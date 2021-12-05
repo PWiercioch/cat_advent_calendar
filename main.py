0e056179a1eff930ad961b0c2a18f1158349a0cf
Pawel W <pawiercioch@gmail.com>
2021-12-05 22:36:07 +0100

    Add date handling: - new buttons available each day - different picture for every day

--- main.py
+++ main.py
@@ -11,29 +11,42 @@ class MainWindow(Screen):
     def __init__(self, **kwargs):
         super(MainWindow, self).__init__(**kwargs)
 
+        self.day, self.month, self.year = self.get_date()
+
         self.outside = GridLayout()
         self.outside.cols = 1
 
-        self.outside.add_widget(Label(text='Hello', size_hint_y=None, height=100, font_size=25))
+        self.outside.add_widget(Label(text=f'Hello to year {self.year} advent cat calendar', size_hint_y=None, height=100, font_size=25))
 
         self.inside = GridLayout()
         self.inside.cols = 7
 
         for n in range(24):
             my_button1 = Button(text=f"Day {n+1}")
+            my_button1.value = n +1
             my_button1.bind(on_release=self.changer)
-            self.inside.add_widget(my_button1)
+            if self.month != 12 or n+1 <= self.day:
+                self.inside.add_widget(my_button1)
+                self.ids[n] = my_button1
+            else:
+                self.inside.add_widget(Label(text=f"Day {n+1}"))
 
         self.outside.add_widget(self.inside)
         self.add_widget(self.outside)
 
 
-    def changer(self, *args):
+    def changer(self, button, *args):
+        self.manager.get_screen('second').ids['picture'].source = str(button.value) + '.jpg'
         self.manager.current = 'second'
         self.manager.transition.direction = 'left'
 
     def get_date(self):
-        pass
+        today = date.today()
+        day = today.strftime('%d')
+        month = today.strftime('%m')
+        year = today.strftime('%Y')
+
+        return int(day), int(month), int(year)
 
 
 class SecondWindow(Screen):
@@ -43,7 +56,10 @@ class SecondWindow(Screen):
         self.grid = GridLayout()
         self.grid.cols = 1
 
-        self.grid.add_widget(Image(source='1.jpg'))
+        picture = Image(source=f'1.jpg')
+        self.grid.add_widget(Label(text=f"Meow", size_hint_y=None, height=100, font_size=15))
+        self.grid.add_widget(picture)
+        self.ids['picture'] = picture
 
         button = Button(text="To main page", size_hint_y=None, height=100, font_size=25)
         button.bind(on_release=self.changer)
@@ -56,7 +72,6 @@ class SecondWindow(Screen):
         self.manager.transition.direction = 'right'
 
 
-
 class WindowManager(ScreenManager):
     def __init__(self, **kwargs):
         super(WindowManager, self).__init__(**kwargs)
@@ -64,7 +79,6 @@ class WindowManager(ScreenManager):
         screen2 = SecondWindow(name='second')
         self.add_widget(screen1)
         self.add_widget(screen2)
-
 
 
 class MyApp(App):
